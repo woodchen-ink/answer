4cb2543b4188c7d482b4d6ab4600752a5d0a0017
wood <wood@czl.net>
2024-08-14 19:57:16 +0800

    修改侧边栏, 添加项目专区, 修改部分翻译

--- ui/src/components/SideNav/index.tsx
+++ ui/src/components/SideNav/index.tsx
@@ -1,31 +1,12 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-
-import { FC } from 'react';
-import { Col, Nav } from 'react-bootstrap';
+import { FC, useState } from 'react';
+import { Col, Nav, Collapse } from 'react-bootstrap';
 import { NavLink, useLocation, useNavigate } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 
 import classnames from 'classnames';
 
 import { loggedUserInfoStore, sideNavStore } from '@/stores';
-import { Icon } from '@/components';
+import { Icon } from '@/components'; // 假设你的项目中已经有一个Icon组件
 import './index.scss';
 
 // 自定义 NavLink 组件
@@ -47,7 +28,7 @@ const CustomNavLink: FC<CustomNavLinkProps> = ({
   <NavLink
     to={to}
     className={({ isActive }) =>
-      classnames('nav-link d-flex align-items-center', {
+      classnames('nav-link d-flex align-items-center custom-nav-link', {
         active: isActive || active,
       })
     }
@@ -56,12 +37,18 @@ const CustomNavLink: FC<CustomNavLinkProps> = ({
     <span className="text-truncate">{children}</span>
   </NavLink>
 );
+
 const Index: FC = () => {
   const { t } = useTranslation();
   const navigate = useNavigate();
   const { pathname } = useLocation();
   const { user: userInfo } = loggedUserInfoStore();
   const { visible, can_revision, revision } = sideNavStore();
+
+  // State to control collapsible sections
+  const [interestOpen, setInterestOpen] = useState(true);
+  const [functionOpen, setFunctionOpen] = useState(true);
+  const [projectOpen, setProjectOpen] = useState(true);
 
   const handleNavClick = (e, path) => {
     e.preventDefault();
@@ -111,79 +98,128 @@ const Index: FC = () => {
             {t('header.nav.public-good')}
           </CustomNavLink>
 
-          {/* 专区 */}
-          <div className="py-2 px-2 mt-3 small fw-bold">
-            {t('header.nav.prefecture')}
+          {/* 兴趣专区 */}
+          <div
+            className="py-2 px-2 mt-3 small fw-bold d-flex justify-content-between align-items-center"
+            onClick={() => setInterestOpen(!interestOpen)}
+            style={{ cursor: 'pointer' }}>
+            {t('header.nav.interest')}
+            <Icon
+              name="chevron-down"
+              className={classnames('transition-transform', {
+                'rotate-180': interestOpen,
+              })}
+            />
           </div>
-          {/* 游戏 */}
-          <CustomNavLink to="/tags/game" icon="controller">
-            {t('header.nav.game')}
-          </CustomNavLink>
-          {/* 音乐 */}
-          <CustomNavLink to="/tags/music" icon="music-note">
-            {t('header.nav.music')}
-          </CustomNavLink>
-          {/* 电影 */}
-          <CustomNavLink to="/tags/video" icon="film">
-            {t('header.nav.video')}
-          </CustomNavLink>
-          {/* 阅读 */}
-          <CustomNavLink to="/tags/read" icon="book">
-            {t('header.nav.read')}
-          </CustomNavLink>
-          {/* 运动 */}
-          <CustomNavLink to="/tags/sports" icon="emoji-sunglasses">
-            {t('header.nav.sports')}
-          </CustomNavLink>
-          {/* 旅游 */}
-          <CustomNavLink to="/tags/travel" icon="globe-asia-australia">
-            {t('header.nav.travel')}
-          </CustomNavLink>
-          {/* 生活 */}
-          <CustomNavLink to="/tags/life" icon="house-heart">
-            {t('header.nav.life')}
-          </CustomNavLink>
-          {/* 吃瓜 */}
-          <CustomNavLink to="/tags/gossip" icon="chat-quote">
-            {t('header.nav.gossip')}
-          </CustomNavLink>
-          {/* 分割线 */}
-          <hr className="my-2 mx-1" />
-          {/* IT */}
-          <CustomNavLink to="/tags/it996007" icon="code-slash">
-            {t('header.nav.it')}
-          </CustomNavLink>
-          {/* 虚拟币 */}
-          <CustomNavLink to="/tags/virtual-currency" icon="coin">
-            {t('header.nav.virtual-currency')}
-          </CustomNavLink>
-          {/* 服务器 */}
-          <CustomNavLink to="/tags/server" icon="hdd-stack">
-            {t('header.nav.server')}
-          </CustomNavLink>
-          {/* 银行和货币 */}
-          <CustomNavLink to="/tags/banks-money" icon="bank">
-            {t('header.nav.banks-money')}
-          </CustomNavLink>
-          {/* 贸易与物流 */}
-          <CustomNavLink to="/tags/trade-logistics" icon="buildings">
-            {t('header.nav.trade-logistics')}
-          </CustomNavLink>
-          {/* 羊毛,交易,拼单和推广 */}
-          <CustomNavLink to="/tags/wool" icon="scissors">
-            {t('header.nav.wool')}
-          </CustomNavLink>
-          <CustomNavLink to="/tags/transaction" icon="currency-exchange">
-            {t('header.nav.transaction')}
-          </CustomNavLink>
-          <CustomNavLink to="/tags/group-purchase" icon="cart-plus">
-            {t('header.nav.group-purchase')}
-          </CustomNavLink>
-          <CustomNavLink to="/tags/promotion" icon="broadcast">
-            {t('header.nav.promotion')}
-          </CustomNavLink>
+          <Collapse in={interestOpen}>
+            <div>
+              {/* 游戏 */}
+              <CustomNavLink to="/tags/game" icon="controller">
+                {t('header.nav.game')}
+              </CustomNavLink>
+              {/* 音乐 */}
+              <CustomNavLink to="/tags/music" icon="music-note">
+                {t('header.nav.music')}
+              </CustomNavLink>
+              {/* 电影 */}
+              <CustomNavLink to="/tags/video" icon="film">
+                {t('header.nav.video')}
+              </CustomNavLink>
+              {/* 阅读 */}
+              <CustomNavLink to="/tags/read" icon="book">
+                {t('header.nav.read')}
+              </CustomNavLink>
+              {/* 运动 */}
+              <CustomNavLink to="/tags/sports" icon="emoji-sunglasses">
+                {t('header.nav.sports')}
+              </CustomNavLink>
+              {/* 旅游 */}
+              <CustomNavLink to="/tags/travel" icon="globe-asia-australia">
+                {t('header.nav.travel')}
+              </CustomNavLink>
+              {/* 生活 */}
+              <CustomNavLink to="/tags/life" icon="house-heart">
+                {t('header.nav.life')}
+              </CustomNavLink>
+              {/* 吃瓜 */}
+              <CustomNavLink to="/tags/gossip" icon="chat-quote">
+                {t('header.nav.gossip')}
+              </CustomNavLink>
+            </div>
+          </Collapse>
 
-          <div className="py-2 px-1  small flex-warprap">
+          {/* 功能专区 */}
+          <div
+            className="py-2 px-2 mt-3 small fw-bold d-flex justify-content-between align-items-center"
+            onClick={() => setFunctionOpen(!functionOpen)}
+            style={{ cursor: 'pointer' }}>
+            {t('header.nav.function')}
+            <Icon
+              name="chevron-down"
+              className={classnames('transition-transform', {
+                'rotate-180': functionOpen,
+              })}
+            />
+          </div>
+          <Collapse in={functionOpen}>
+            <div>
+              {/* IT */}
+              <CustomNavLink to="/tags/it996007" icon="code-slash">
+                IT
+              </CustomNavLink>
+              {/* 虚拟币 */}
+              <CustomNavLink to="/tags/virtual-currency" icon="coin">
+                {t('header.nav.virtual-currency')}
+              </CustomNavLink>
+              {/* 服务器 */}
+              <CustomNavLink to="/tags/server" icon="hdd-stack">
+                {t('header.nav.server')}
+              </CustomNavLink>
+              {/* 银行和货币 */}
+              <CustomNavLink to="/tags/banks-money" icon="bank">
+                {t('header.nav.banks-money')}
+              </CustomNavLink>
+              {/* 贸易与物流 */}
+              <CustomNavLink to="/tags/trade-logistics" icon="buildings">
+                {t('header.nav.trade-logistics')}
+              </CustomNavLink>
+              {/* 羊毛,交易,拼单和推广 */}
+              <CustomNavLink to="/tags/wool" icon="scissors">
+                {t('header.nav.wool')}
+              </CustomNavLink>
+              <CustomNavLink to="/tags/transaction" icon="currency-exchange">
+                {t('header.nav.transaction')}
+              </CustomNavLink>
+              <CustomNavLink to="/tags/group-purchase" icon="cart-plus">
+                {t('header.nav.group-purchase')}
+              </CustomNavLink>
+              <CustomNavLink to="/tags/promotion" icon="broadcast">
+                {t('header.nav.promotion')}
+              </CustomNavLink>
+            </div>
+          </Collapse>
+
+          {/* 项目专区 */}
+          <div
+            className="py-2 px-2 mt-3 small fw-bold d-flex justify-content-between align-items-center"
+            onClick={() => setProjectOpen(!projectOpen)}
+            style={{ cursor: 'pointer' }}>
+            {t('header.nav.project')}
+            <Icon
+              name="chevron-down"
+              className={classnames('transition-transform', {
+                'rotate-180': projectOpen,
+              })}
+            />
+          </div>
+          <Collapse in={projectOpen}>
+            <div>
+              {/* One Hub */}
+              <CustomNavLink to="/tags/one-hub">One Hub</CustomNavLink>
+            </div>
+          </Collapse>
+
+          <div className="mt-2 py-2 px-1  small flex-warprap">
             <hr className="mt-0 mb-2" />
             <a href="/questions/D1C2">{t('header.nav.introduction')}</a>
             <br />
